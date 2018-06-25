63d2092637bcd105b83988a5085794df039f3a33
Arthur Barros <arthurbarros@arthurs-mbp-97.go.sebrae.corp>
2018-06-25 15:40:06 -0300

    Sync

--- SEBRAE/forms/32074 - Lista de Recursos/custom.js
+++ SEBRAE/forms/32074 - Lista de Recursos/custom.js
@@ -1,3 +1,10 @@
 function carregaTable(table){
     var row = wdkAddChild(table);
 }
+
+function sincRegistros() {
+    var dataset = DatasetFactory.getDataset("sebrae_cadastra_recursos", null, null, null);
+    for(var i in dataset) {
+        console.log(dataset.values);
+    }
+}
