1bc4ad3486b470a096f303e806cf2108ca2d13ad
Arthur Barros <arthurbarros@arthurs-mbp-97.go.sebrae.corp>
2018-06-26 17:04:42 -0300

    Sync

--- SEBRAE/datasets/recursosGrupos.js
+++ SEBRAE/datasets/recursosGrupos.js
@@ -2,6 +2,18 @@ function createDataset(fields, constraints, sortFields) {
     var c1 = DatasetFactory.createConstraint("groupId", "%REC-%", "%REC-%", ConstraintType.MUST)
     c1.setLikeSearch(true);
     var c = new Array(c1);
+
+    if (constraints != null) {
+        for (var i = 0; i < constraints.length; i++) {
+            log.info('--DATASET GRUPO RECURSO cons: ' + constraints[i].initialValue);
+            if (constraints[i].fieldName == "groupDescription" || constraints[i].fieldName == "groupId") {
+                var c1 = DatasetFactory.createConstraint("groupDescription", constraints[i].initialValue, constraints[i].initialValue, ConstraintType.MUST);
+                c1.setLikeSearch(true);
+                c.push(c1);
+            }
+        }
+    }
+
     var dataset = DatasetFactory.getDataset("group", null, c, null);
     log.info('--DATASET GRUPO RECURSO');
     return dataset;
